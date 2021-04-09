ef358136c1bb19a2776b41734012e8e7bc6142ec
PlaneB22 <47218421+petruzzellialessandro@users.noreply.github.com>
2021-04-09 12:06:38 +0200

    Check already loaded models

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import queue
 import threading
+import time
 
 import gensim.parsing.preprocessing as pp
 
@@ -64,11 +65,15 @@ def select_model(selected_model):
 
     if selected_model == 1 or selected_model == 2:
         global __doc2vec, __most_similar
-        __doc2vec = None
-        __returned_queue = queue.Queue()
-        thread = threading.Thread(target=d2v.load_model,
-                                  args=(__tokenized_plots, "Models/Doc2Vec/doc2vec_model", __returned_queue))
-        thread.start()
+        try:
+            if __doc2vec is not None:
+                print("Already Loaded")
+        except Exception:
+            __doc2vec = None
+            __returned_queue = queue.Queue()
+            thread = threading.Thread(target=d2v.load_model,
+                                      args=(__tokenized_plots, "Models/Doc2Vec/doc2vec_model", __returned_queue))
+            thread.start()
         if selected_model == 1:
             __most_similar = True
         else:
@@ -77,43 +82,69 @@ def select_model(selected_model):
 
     if selected_model == 3 or selected_model == 4:
         global __word2vec, __w2c_pre_trained
-        __word2vec = None
         if selected_model == 3:
             __w2c_pre_trained = True
         else:
             __w2c_pre_trained = False
-        __returned_queue = queue.Queue()
-        thread = threading.Thread(target=w2v.load_model, args=(__tokenized_plots, "Models\Word2Vec\word2vec_model",
-                                                               __w2c_pre_trained, __returned_queue))
-        thread.start()
+        try:
+            if __word2vec is not None and __id_Model == selected_model:
+                print("Already Loaded")
+            else:
+                raise Exception
+        except Exception:
+            __word2vec = None
+            __returned_queue = queue.Queue()
+            thread = threading.Thread(target=w2v.load_model, args=(__tokenized_plots, "Models\Word2Vec\word2vec_model",
+                                                                   __w2c_pre_trained, __returned_queue))
+            thread.start()
         __id_Model = selected_model
 
     if selected_model == 5 or selected_model == 6:
         global __fasttext, __ft_pre_trained
-        __fasttext = None
         __returned_queue = queue.Queue()
         if selected_model == 5:
             __ft_pre_trained = True
-            thread = threading.Thread(target=ft.create_model_fasttext_fb, args=(__fasttext, __returned_queue))
-            thread.start()
+            try:
+                if __fasttext is not None and selected_model == __id_Model:
+                    print("Already Loaded")
+                else:
+                    raise Exception
+            except Exception:
+                __fasttext = None
+                thread = threading.Thread(target=ft.create_model_fasttext_fb, args=(__fasttext, __returned_queue))
+                thread.start()
         else:
             __ft_pre_trained = False
-            thread = threading.Thread(target=ft.load_model, args=(__tokenized_plots, "Models/FastText/fasttext_model",
-                                                                  __returned_queue))
-            thread.start()
+            try:
+                if __fasttext is not None and selected_model == __id_Model:
+                    print("Already Loaded")
+                else:
+                    raise Exception
+            except Exception:
+                __fasttext = None
+                thread = threading.Thread(target=ft.load_model, args=(__tokenized_plots, "Models/FastText"
+                                                                                         "/fasttext_model",
+                                                                      __returned_queue))
+                thread.start()
         __id_Model = selected_model
 
     if selected_model == 7:
         global __tfidf_model, __tfidf_index, __tfidf_dictionary
-        __tfidf_model = None
-        __tfidf_index = None
-        __tfidf_dictionary = None
-        __returned_queue = queue.Queue()
-        thread = threading.Thread(target=tfidf.load_tfidf_model, args=(__tokenized_plots, "Models/TFIDF/tfidf_model",
-                                                                       "Models/TFIDF/matrix_tfidf",
-                                                                       "Models/TFIDF/dictionary_tfidf",
-                                                                       __returned_queue))
-        thread.start()
+        try:
+            if __tfidf_model is not None and __tfidf_index is not None and __tfidf_dictionary is not None:
+                print("Already Loaded")
+            else:
+                raise Exception
+        except Exception:
+            __tfidf_model = None
+            __tfidf_index = None
+            __tfidf_dictionary = None
+            __returned_queue = queue.Queue()
+            thread = threading.Thread(target=tfidf.load_tfidf_model, args=(__tokenized_plots, "Models/TFIDF/tfidf_model",
+                                                                           "Models/TFIDF/matrix_tfidf",
+                                                                           "Models/TFIDF/dictionary_tfidf",
+                                                                           __returned_queue))
+            thread.start()
         __id_Model = selected_model
 
 
@@ -138,30 +169,54 @@ def __get_rec(IDs_pref, tokenized_pref):
     global __returned_queue
     if __id_Model == 1 or __id_Model == 2:
         global __doc2vec, __most_similar
-        __doc2vec = __returned_queue.get()
-        recommands = d2v.print_res_doc2vec(token_strings=tokenized_pref, documents=__tokenized_plots,
+        try:
+            if __doc2vec is not None:
+                print("Already Loaded")
+            else:
+                raise Exception
+        except Exception:
+            __doc2vec = __returned_queue.get()
+        recommends = d2v.print_res_doc2vec(token_strings=tokenized_pref, documents=__tokenized_plots,
                                            titles=__films_titles, IDs=__films_IDs, modelDoC=__doc2vec,
                                            most_similar=__most_similar, prefIDs=IDs_pref)
     elif __id_Model == 3 or __id_Model == 4:
         global __word2vec, __w2c_pre_trained
-        __word2vec = __returned_queue.get()
-        recommands = w2v.print_res_word2vec(token_strings=tokenized_pref, documents=__tokenized_plots,
+        try:
+            if __word2vec is not None:
+                print("Already Loaded")
+            else:
+                raise Exception
+        except Exception:
+            __word2vec = __returned_queue.get()
+        recommends = w2v.print_res_word2vec(token_strings=tokenized_pref, documents=__tokenized_plots,
                                             titles=__films_titles, IDs=__films_IDs, modelWord=__word2vec,
                                             pretrained=__w2c_pre_trained, prefIDs=IDs_pref)
     elif __id_Model == 5 or __id_Model == 6:
         global __fasttext, __ft_pre_trained
-        __fasttext = __returned_queue.get()
-        recommands = ft.print_res_fastText(token_strings=tokenized_pref, documents=__tokenized_plots,
+        try:
+            if __fasttext is not None:
+                print("Already Loaded")
+            else:
+                raise Exception
+        except Exception:
+            __fasttext = __returned_queue.get()
+        recommends = ft.print_res_fastText(token_strings=tokenized_pref, documents=__tokenized_plots,
                                            titles=__films_titles, IDs=__films_IDs, modelFastText=__fasttext,
                                            pretrained=__ft_pre_trained, prefIDs=IDs_pref)
     elif __id_Model == 7:
         global __tfidf_model, __tfidf_index, __tfidf_dictionary
-        loaded = __returned_queue.get()
-        __tfidf_model = loaded[0]
-        __tfidf_index = loaded[1]
-        __tfidf_dictionary = loaded[2]
-        recommands = tfidf.print_res_tfidf(token_strings=tokenized_pref, documents=__tokenized_plots,
+        try:
+            if __tfidf_model is not None and __tfidf_index is not None and __tfidf_dictionary is not None:
+                print("Already Loaded")
+            else:
+                raise Exception
+        except Exception:
+            loaded = __returned_queue.get()
+            __tfidf_model = loaded[0]
+            __tfidf_index = loaded[1]
+            __tfidf_dictionary = loaded[2]
+        recommends = tfidf.print_res_tfidf(token_strings=tokenized_pref, documents=__tokenized_plots,
                                            titles=__films_titles, IDs=__films_IDs, dictionary=__tfidf_dictionary,
                                            tfidfmodel=__tfidf_model, index=__tfidf_index, prefIDs=IDs_pref)
-    return recommands
+    return recommends
 
